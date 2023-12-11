07d002ba277c25b84c7fc689cd749c9c3b6e2fe0
yudai-suzuki-ars <yudai.suzuki@arsaga.jp>
2023-12-11 13:59:17 +0900

    fix

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { projects } from '../utils/projectsData';
 
 const Home = () => (
   <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
+    <h1>What Can I Deploy to Static Uyudane?</h1>
     <div className="card-grid">
       {projects.map((project) => {
         const Icon = projectIcons[project.id];
