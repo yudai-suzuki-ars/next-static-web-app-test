2cba372078f10150753268544d4f237055a72a09
yudai-suzuki-ars <yudai.suzuki@arsaga.jp>
2023-12-11 13:51:06 +0900

    fix

--- utils/projectsData.js
+++ utils/projectsData.js
@@ -1,7 +1,7 @@
 const projects = [
   {
     id: 'uyudane',
-    name: 'React',
+    name: 'uyudane',
     path: 'facebook/react',
     slug: 'facebook-react',
   },
@@ -12,7 +12,7 @@ const projects = [
     path: 'sveltejs/svelte',
     slug: 'sveltejs-svelte',
   },
-  { id: 'next', name: 'Next.js', path: 'zeit/next.js', slug: 'zeit-nextjs' },
+  { id: 'next', name: 'uyudane.js', path: 'zeit/next.js', slug: 'zeit-nextjs' },
   { id: 'nuxt', name: 'Nuxt.js', path: 'nuxt/nuxt.js', slug: 'nuxt-nuxtjs' },
   {
     id: 'gatsby',
